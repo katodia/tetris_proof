a04dcc96fe26dbe744a4558f26acf4e365c08c6f
Ruben del Blanco <rubendelblanco@gmail.com>
2023-07-22 23:10:25 +0200

    final commit. Demo done!

--- main.py
+++ main.py
@@ -5,10 +5,13 @@ from Shape import Shape
 pygame.init()
 
 # Set up the drawing window
-screen = pygame.display.set_mode([800, 600])
+screen = pygame.display.set_mode([1024, 768])
 square_width = 30
-rows = 4
+rows = 8
 cols = 5
+#posicion inicial x,y desde la que se colocaran las piezas
+x_position = 120
+y_position = 40
 
 # Run until the user asks to quit
 running = True
@@ -16,14 +19,15 @@ shapes = []
 previous_pos = pygame.mouse.get_pos()
 
 for i in range(5):
-    shapes.append(Shape(screen, 220 + (i * 90), 80, i))
+    shapes.append(Shape(screen, (cols*square_width) + 40 + (i * x_position), y_position, i))
+    shapes.append(Shape(screen, (cols*square_width) + 40 + (i * x_position), y_position + 150, i)) #comentar esta linea si solo se quiere una fila de piezas
 
 pygame.event.set_grab(True)
 
 
-def draw_matrix(square_width, rows, cols):
-    for i in range(rows):
-        for j in range(cols):
+def draw_matrix(square_width, cols, rows):
+    for i in range(cols):
+        for j in range(rows):
             pygame.draw.rect(screen, (0, 0, 0),
                              pygame.Rect(20 + ((square_width + 1) * i), 20 + (square_width + 1) * j, square_width,
                                          square_width))
@@ -65,7 +69,7 @@ while running:
 
     # Fill the background with white
     screen.fill((255, 255, 255))
-    draw_matrix(square_width, rows, cols)
+    draw_matrix(square_width, cols, rows)
     for i in range(len(shapes)):
         shapes[i].draw()
     # Flip the display
